5036d3138729e9186590934db229d875a9cf4c28
acurtright <curtright.a@husky.neu.edu>
2020-03-30 12:56:07 -0700

    Modified to run of csv file instead of db connection

--- Climate_tree_scraper.py
+++ Climate_tree_scraper.py
@@ -7,33 +7,32 @@ import warnings
 import sys
 import random
 import json
+import csv
 
-def getNameTable():
-    host="localhost"
-    dbname="climateTree"
-    user="postgres"
-    password="700813"
-    conn_str='host={0} user={1} dbname={2} password={3}'.format(host, user, dbname, password)
-    conn=psycopg2.connect(conn_str)
-    cur=conn.cursor()
-    cur.execute('select "NAME".name,"PLACE_INFO".place_id from "NAME_PLACE" inner join "PLACE_INFO" on "NAME_PLACE".place_id = "PLACE_INFO".place_id'
-    ' inner join "NAME" on "NAME".name_id = "NAME_PLACE".name_id order by "PLACE_INFO".population desc')
-    return cur.fetchall()
 
-array=[]
+def parsePlaceCSV():
+    print('parsing csv')
+    with open('place_name_id.csv', mode='r', encoding='utf8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        result = []
+        for row in reader:
+            result.append([row['place'], row['id']])
+    return result
 
-def getQuery(name,tup):
-    return name+" climate change"
+def getQuery(name):
+    print('getting query')
+    return name+ " climate change"
    
 def writeToJson(obj,num):
-    with open('../climateChange/json'+str(num)+".json","w+",encoding='utf-8') as f:
+    with open('./output/' + str(num)+".json","w+",encoding='utf-8') as f:
         json.dump(obj,f)
 
-def getJson(name,tup,place_id,link):
+def getJson(place_id,link):
+    print('getting preview')
     data={}
     data['user_id']=0
     data['hyperlink']=link
-    title,desc,image=web_preview(link, timeout=1000)
+    title,desc,image=web_preview(link, timeout=100)
     data['story_title']=title
     data['description']=desc
     data['rating']=0
@@ -47,21 +46,25 @@ def getJson(name,tup,place_id,link):
     return data
 
 warnings.filterwarnings("ignore")
-tmp=getNameTable()
-# to search
+
+tmp = parsePlaceCSV()
 num=0
-for row in tmp : #do iterate for each place
+print("starting loop")
+for row in tmp: #do iterate for each place
+    print(row, flush=True)
     num+=1
     print(num)
     if num>50000:
         break
     tup=[]
-   
-    query=getQuery(row[0],tup) #row[0] stands for the name of place
+    placeName = row[0]
+    if not placeName:
+       continue
+    query=getQuery(placeName) #row[0] stands for the name of place
     try:
         tmpJson=[]
-        for j in search(query,stop=1):
-            tmpJson.append(getJson(row[0],tup,row[1],j)) #row[1] stands for the place_id
+        for link in search(query,stop=1):
+            tmpJson.append(getJson(row[1],link)) #row[1] stands for the place_id
         writeToJson(tmpJson,num)
     except:
-        print("ERROR",num, sys.exc_info()[0])
+        print("ERROR",num, sys.exc_info()[0].reason)
